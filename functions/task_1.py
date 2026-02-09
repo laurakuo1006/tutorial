fb41348364e2800381301a85e4057b05dc6372d5
Laura Kuo <laurak10006@gmail.com>
2026-02-08 21:53:58 -0800

    update functions/task_1.py

--- functions/task_1.py
+++ functions/task_1.py
@@ -1,16 +1,12 @@
 import os
-import pandas as pd
-import numpy as np
+import csv
+import random
 
-def task_1(output_folder='data', output_1='dataset1.csv', output_2='dataset2.csv'):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    length = 100
-    dataset1 = np.random.rand(length)
-    dataset2 = np.random.rand(length)
-    df1 = pd.DataFrame(dataset1, columns=['Value'])
-    df2 = pd.DataFrame(dataset2, columns=['Value'])
-    df1.to_csv(os.path.join(output_folder, output_1), index=False)
-    df2.to_csv(os.path.join(output_folder, output_2), index=False)
-    faasr_put_file(local_folder=output_folder, local_file='dataset1.csv', remote_folder='tutorial', remote_file='dataset1.csv')
-    faasr_put_file(local_folder=output_folder, local_file='dataset2.csv', remote_folder='tutorial', remote_file='dataset2.csv')
+def task_1(folder='tutorial', output1='random_integers_1.csv'):
+    os.makedirs(folder, exist_ok=True)
+    path_out1 = os.path.join(folder, output1)
+    with open(path_out1, mode='w', newline='') as f1:
+        writer = csv.writer(f1)
+        for _ in range(20):
+            writer.writerow([random.randint(-1000, 1000)])
+    faasr_put_file(local_file='random_integers_1.csv', remote_folder=folder, remote_file='random_integers_1.csv')
