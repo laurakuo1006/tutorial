ce692dad7d1ae54c4780a2320726d0fb7fce6ab6
Laura Kuo <laurak10006@gmail.com>
2026-02-10 22:36:01 -0800

    update functions/task_3.py

--- functions/task_3.py
+++ functions/task_3.py
@@ -1,39 +1,28 @@
 import os
 import csv
 
-def task_3(folder='tutorial', input1='dataset1.csv', input2='dataset2.csv', output1='summed_data.csv'):
+def task_3(folder='tutorial', input1='dataset1.csv', input2='dataset2.csv', output1='combined_dataset.csv'):
     faasr_get_file(remote_folder=folder, remote_file='dataset1.csv', local_file='dataset1.csv')
     faasr_get_file(remote_folder=folder, remote_file='dataset2.csv', local_file='dataset2.csv')
     os.makedirs(folder, exist_ok=True)
     path1 = os.path.join(folder, input1)
     path2 = os.path.join(folder, input2)
-    out_path = os.path.join(folder, output1)
+    output_path = os.path.join(folder, output1)
     with open(path1, newline='') as f1:
-        reader1 = csv.DictReader(f1)
-        if reader1.fieldnames != ['value']:
-            raise ValueError(f"{input1} header must be ['value']")
-        data1 = [row['value'] for row in reader1]
-    if len(data1) != 20:
-        raise ValueError(f'{input1} must contain exactly 20 rows')
-    try:
-        data1_int = [int(x) for x in data1]
-    except:
-        raise ValueError(f'{input1} must contain only integers')
+        reader1 = csv.reader(f1)
+        header1 = next(reader1, None)
+        data1 = [float(row[0]) for row in reader1 if row]
     with open(path2, newline='') as f2:
-        reader2 = csv.DictReader(f2)
-        if reader2.fieldnames != ['value']:
-            raise ValueError(f"{input2} header must be ['value']")
-        data2 = [row['value'] for row in reader2]
-    if len(data2) != 20:
-        raise ValueError(f'{input2} must contain exactly 20 rows')
-    try:
-        data2_int = [int(x) for x in data2]
-    except:
-        raise ValueError(f'{input2} must contain only integers')
-    summed = [a + b for a, b in zip(data1_int, data2_int)]
-    with open(out_path, 'w', newline='') as fout:
+        reader2 = csv.reader(f2)
+        header2 = next(reader2, None)
+        data2 = [float(row[0]) for row in reader2 if row]
+    min_len = min(len(data1), len(data2))
+    data1 = data1[:min_len]
+    data2 = data2[:min_len]
+    combined = [a + b for a, b in zip(data1, data2)]
+    with open(output_path, 'w', newline='') as fout:
         writer = csv.writer(fout)
-        writer.writerow(['summed_value'])
-        for val in summed:
+        writer.writerow(['sum'])
+        for val in combined:
             writer.writerow([val])
-    faasr_put_file(local_file='summed_data.csv', remote_folder=folder, remote_file='summed_data.csv')
+    faasr_put_file(local_file='combined_dataset.csv', remote_folder=folder, remote_file='combined_dataset.csv')
