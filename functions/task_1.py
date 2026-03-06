fd7d9fb1a24f94678878324f74acd359f0d5a035
Laura Kuo <laurak10006@gmail.com>
2026-03-05 20:00:36 -0800

    update functions/task_1.py

--- functions/task_1.py
+++ functions/task_1.py
@@ -2,19 +2,20 @@ from FaaSr_py.client.py_client_stubs import faasr_put_file, faasr_get_file
 import os
 import random
 
-def task_1(folder='tutorial', output1='integers_a.csv', output2='integers_b.csv'):
+def task_1(folder='tutorial', output1='file1.csv', output2='file2.csv'):
     os.makedirs(folder, exist_ok=True)
-    path_a = os.path.join(folder, output1)
-    path_b = os.path.join(folder, output2)
-    integers_a = [random.randint(1, 100) for _ in range(10)]
-    integers_b = [random.randint(1, 100) for _ in range(10)]
-    with open(path_a, 'w') as f:
-        for val in integers_a:
-            f.write(f'{val}\n')
-    with open(path_b, 'w') as f:
-        for val in integers_b:
-            f.write(f'{val}\n')
-    print(f'Generated {path_a} with values: {integers_a}')
-    print(f'Generated {path_b} with values: {integers_b}')
-    faasr_put_file(local_file='tutorial/integers_a.csv', remote_folder=folder, remote_file='integers_a.csv')
-    faasr_put_file(local_file='tutorial/integers_b.csv', remote_folder=folder, remote_file='integers_b.csv')
+    random.seed(42)
+    values1 = [random.randint(1, 100) for _ in range(10)]
+    values2 = [random.randint(1, 100) for _ in range(10)]
+    path1 = os.path.join(folder, output1)
+    path2 = os.path.join(folder, output2)
+    with open(path1, 'w') as f:
+        for v in values1:
+            f.write(f'{v}\n')
+    with open(path2, 'w') as f:
+        for v in values2:
+            f.write(f'{v}\n')
+    faasr_put_file(local_file='tutorial/file1.csv', remote_folder=folder, remote_file='file1.csv')
+    faasr_put_file(local_file='tutorial/file2.csv', remote_folder=folder, remote_file='file2.csv')
+    print(f'Written {path1} with values: {values1}')
+    print(f'Written {path2} with values: {values2}')
