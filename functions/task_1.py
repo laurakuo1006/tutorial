e4d23a70d397bd2de8a0a19673dd53793e6c8d01
Laura Kuo <laurak10006@gmail.com>
2026-03-05 19:53:33 -0800

    update functions/task_1.py

--- functions/task_1.py
+++ functions/task_1.py
@@ -1,33 +1,20 @@
 from FaaSr_py.client.py_client_stubs import faasr_put_file, faasr_get_file
 import os
 import random
-import csv
 
-def task_1(folder='tutorial', output1='source_a.csv', output2='source_b.csv'):
+def task_1(folder='tutorial', output1='integers_a.csv', output2='integers_b.csv'):
     os.makedirs(folder, exist_ok=True)
-    seed_a = random.randint(1, 10 ** 9)
-    rng_a = random.Random(seed_a)
-    print(f'source_a.csv seed used: {seed_a}')
-    values_a = [rng_a.randint(1, 100) for _ in range(10)]
-    print(f'source_a values: {values_a}')
     path_a = os.path.join(folder, output1)
-    with open(path_a, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['value'])
-        for v in values_a:
-            writer.writerow([v])
-    seed_b = seed_a
-    while seed_b == seed_a:
-        seed_b = random.randint(1, 10 ** 9)
-    rng_b = random.Random(seed_b)
-    print(f'source_b.csv seed used: {seed_b}')
-    values_b = [rng_b.randint(1, 100) for _ in range(10)]
-    print(f'source_b values: {values_b}')
     path_b = os.path.join(folder, output2)
-    with open(path_b, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['value'])
-        for v in values_b:
-            writer.writerow([v])
-    faasr_put_file(local_file='tutorial/source_a.csv', remote_folder=folder, remote_file='source_a.csv')
-    faasr_put_file(local_file='tutorial/source_b.csv', remote_folder=folder, remote_file='source_b.csv')
+    integers_a = [random.randint(1, 100) for _ in range(10)]
+    integers_b = [random.randint(1, 100) for _ in range(10)]
+    with open(path_a, 'w') as f:
+        for val in integers_a:
+            f.write(f'{val}\n')
+    with open(path_b, 'w') as f:
+        for val in integers_b:
+            f.write(f'{val}\n')
+    print(f'Generated {path_a} with values: {integers_a}')
+    print(f'Generated {path_b} with values: {integers_b}')
+    faasr_put_file(local_file='tutorial/integers_a.csv', remote_folder=folder, remote_file='integers_a.csv')
+    faasr_put_file(local_file='tutorial/integers_b.csv', remote_folder=folder, remote_file='integers_b.csv')
