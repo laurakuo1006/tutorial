fa7adff2fe101cab0b17c0f0ba8a2bca15e71615
Laura Kuo <laurak10006@gmail.com>
2026-03-05 19:42:51 -0800

    update functions/task_1.py

--- functions/task_1.py
+++ functions/task_1.py
@@ -2,20 +2,25 @@ from FaaSr_py.client.py_client_stubs import faasr_put_file, faasr_get_file
 import os
 import random
 
-def task_1(folder='tutorial', output1='integers_a.csv', output2='integers_b.csv'):
+def task_1(folder='tutorial', output1='file1.csv', output2='file2.csv'):
     os.makedirs(folder, exist_ok=True)
-    path_a = os.path.join(folder, output1)
-    path_b = os.path.join(folder, output2)
-    integers_a = [random.randint(1, 100) for _ in range(10)]
-    integers_b = [random.randint(1, 100) for _ in range(10)]
-    with open(path_a, 'w') as f:
-        f.write('value_a\n')
-        for val in integers_a:
-            f.write(f'{val}\n')
-    with open(path_b, 'w') as f:
-        f.write('value_b\n')
-        for val in integers_b:
-            f.write(f'{val}\n')
-    print(f'Generated {path_a} and {path_b} with 10 random integers each.')
-    faasr_put_file(local_file='tutorial/integers_a.csv', remote_folder=folder, remote_file='integers_a.csv')
-    faasr_put_file(local_file='tutorial/integers_b.csv', remote_folder=folder, remote_file='integers_b.csv')
+    values1 = [random.randint(1, 100) for _ in range(10)]
+    values2 = [random.randint(1, 100) for _ in range(10)]
+    path1 = os.path.join(folder, output1)
+    with open(path1, mode='w', encoding='utf-8', newline='\n') as f:
+        f.write('value\n')
+        for v in values1:
+            f.write(f'{v}\n')
+    path2 = os.path.join(folder, output2)
+    with open(path2, mode='w', encoding='utf-8', newline='\n') as f:
+        f.write('value\n')
+        for v in values2:
+            f.write(f'{v}\n')
+    sums = [v1 + v2 for v1, v2 in zip(values1, values2)]
+    result_path = os.path.join(folder, 'result.csv')
+    with open(result_path, mode='w', encoding='utf-8', newline='\n') as f:
+        f.write('value\n')
+        for s in sums:
+            f.write(f'{s}\n')
+    faasr_put_file(local_file='tutorial/file1.csv', remote_folder=folder, remote_file='file1.csv')
+    faasr_put_file(local_file='tutorial/file2.csv', remote_folder=folder, remote_file='file2.csv')
